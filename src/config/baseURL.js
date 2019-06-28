004e0de37ac01c9badd790040a8ea3d225e226f8
poporeki <yansk@live.com>
2019-06-28 16:48:21 +0800

    '190628'

--- src/config/baseURL.js
+++ src/config/baseURL.js
@@ -4,4 +4,4 @@
 
 // export const BASE_URL = 'http://www.yansk.top';
 
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://v.yansk.cn' : 'http://192.168.199.147:3000';
